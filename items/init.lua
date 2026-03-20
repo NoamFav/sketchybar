62499faa36123ca69553c7981535a632b2cac2cd
NoamFav <95364866+NoamFav@users.noreply.github.com>
2026-03-20 18:00:19 +0100

    feat(ui): added battery, wifi and music widgets; deprecated media module

--- items/init.lua
+++ items/init.lua
@@ -5,6 +5,9 @@ if minimal then
 	require("items.menus")
 	require("items.aerospace_workspaces")
 	require("items.calendar")
+	require("items.widgets.battery")
+	require("items.widgets.wifi")
+	require("items.widgets.music")
 else
 	require("items.apple")
 	require("items.menus")
@@ -12,5 +15,5 @@ else
 	require("items.front_app")
 	require("items.calendar")
 	require("items.widgets")
-	require("items.media")
+	-- require("items.media") deprecated rn
 end
